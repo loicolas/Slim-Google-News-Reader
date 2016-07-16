6635696453326f3981290340266bedfd77faa667
loicolas <loicolas@gmail.com>
2016-07-16 11:05:19 +0200

    add login route and template

--- src/routes.php
+++ src/routes.php
@@ -1,13 +1,37 @@
 <?php
 // Routes
 
+$app->post('/auth', function ($request, $response, $args) {
+    // log used route
+    $this->logger->info("get route '".$request->getAttribute('route')->getName()."' with uri '". $request->getUri() ."' ");
+    
+    // initialize view paramaters
+    $view_params = [];
+    
+    return $this->view->render($response, 'login.twig', $view_params);
+    
+})->setName('auth');
+
+$app->get('/login', function ($request, $response, $args) {
+    // log used route
+    $this->logger->info("get route '".$request->getAttribute('route')->getName()."' with uri '". $request->getUri() ."' ");
+    
+    // initialize view paramaters
+    $view_params = [];
+    
+    return $this->view->render($response, 'login.twig', $view_params);
+    
+})->setName('login');
+
 $app->get('/[{feed}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("App'/' route");
+    // log used route
+    $this->logger->info("get route '".$request->getAttribute('route')->getName()."' with uri '". $request->getUri() ."' ");
     $current_feed = $args['feed'] ?? null;
     
+    // get available feeds declared in the configuration
     $available_feeds = $this->get('settings')['news-reader'];
     
+    // initialize view paramaters
     $view_params = [ 
         'current_feed'      => $current_feed,
         'error'             => false, 
@@ -15,16 +39,18 @@ $app->get('/[{feed}]', function ($request, $response, $args) {
         'available_feeds'   => $available_feeds,
         'flash_message'     => $this->flash
     ];
+    
+    // get news reader Service
     $rssNewsReader = $this->get('rssNewsReader');
     
     try {
+        // get content of the news feed
         $view_params['news_lists'] = $rssNewsReader->find($current_feed);
     } catch (\Exception $e){
         $this->flash->addMessage('danger', $e->getMessage());
-        $view_params['error']            = true;
-        $view_params['error_message']    = $e->getMessage(); 
     }
     
     return $this->view->render($response, 'home.twig', $view_params);
     
-});
+})->setName('news');
+
